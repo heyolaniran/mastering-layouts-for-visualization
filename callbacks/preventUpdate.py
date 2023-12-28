c1d324a732fbf4b2a076e16226491a59c02bdc1c
heyolaniran <olaniran.abd@gmail.com>
2023-12-28 23:36:28 +0100

    feat: prevent Update with advanced callbacks

---
+++ callbacks/preventUpdate.py
@@ -0,0 +1,32 @@
+from dash import dcc,  html , callback, Dash , Output , Input
+
+external_style = ['']
+
+app = Dash(__name__ , title='Prevent Update', external_stylesheets=external_style) 
+
+
+app.layout  = html.Div(
+    
+    html.P('Clickez sur le bouton pour afficher') ,
+    
+    dcc.Button(id='button'),
+    
+    html.P(id='show')
+)
+
+@callback(
+    Output(component_id='show', component_property='children'), 
+    Input('button', component_property='n_clicks')
+)
+
+def show_content(clicks) :
+    
+    if clicks is None : 
+        raise PreventUpdate
+    else : 
+        return 'Le texte s\'affiche au click du button'
+
+
+
+if(__name__ == '__main__'): 
+    app.run(debug=True)
