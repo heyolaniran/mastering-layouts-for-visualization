7028b0a0b3236f4b76a63c6a665b7c197cccc1b1
heyolaniran <olaniran.abd@gmail.com>
2023-12-29 19:01:38 +0100

    feat: prime factors TP

---
+++ callbacks/primeFactor.py
@@ -0,0 +1,52 @@
+from dash import Dash ,  html , dcc , callback ,  Output , Input , no_update
+from dash.exceptions import PreventUpdate 
+
+
+styles = ['']
+
+app = Dash(name=__name__ , external_styles=styles, title='Prime Factors') 
+
+app.layout = html.Div([
+    
+    html.P('Insert a number and get his prime factors'),
+    
+    dcc.Input(id='number') , 
+    
+    html.Div(id='result')
+])
+
+@callback(
+    Output('result', component_property='children'), 
+    Input(component_id='number', component_property='value')
+)
+
+def show_prime_factors(number) : 
+    if number is None : 
+        raise PreventUpdate(msg="Insert not null value plz")
+    else : 
+        factors = prime_factors(number)
+    return '{} equals to {}'.format(number , '*'.join(str(factor) for factor in factors))
+
+
+def prime_factors(num) : 
+    if num <= 1 : 
+        raise PreventUpdate(msg='Number must be over or equal to 2') 
+    else : 
+        n = number 
+        i = 2 
+        factors = [] 
+        
+        while n >= i*i : 
+            if n % i == 0: 
+                n = int(n/i)
+                factors.append(i)
+            else : 
+                i+= 1 if i == 2 else 2
+            
+            
+        factors.append(n)
+        
+        return factors
+    
+if __name__ == '__main__' : 
+    app.run(debug=True)
