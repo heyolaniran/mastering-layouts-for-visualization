3643e7d5ebc18f9c1c8aea259aa7905f3ea53967
heyolaniran <olaniran.abd@gmail.com>
2024-01-06 23:37:15 +0100

    feat : displaying selection data , fixed patching children output

--- callbacks/clientSide/patternMatching.py
+++ callbacks/clientSide/patternMatching.py
@@ -1,14 +1,13 @@
-from dash import Dash, html, callback , Output , Input , Patch, dcc
+from dash import Dash, html, callback , Output , Patch, Input , dcc, ALL
 
 
-
-app = Dash(__name__, title="Pattern Matching")
+app = Dash(__name__, title="Pattern Matching", suppress_callback_exceptions=True)
 
 options = ["NYC", "MTL", "LA", "TOKYO"]
 
 app.layout = html.Div([
 
-    html.Button(id="add_filter", title='Ajouter un filtre', n_clicks=0), 
+    html.Button( "Add Filter" , id="add_filter", n_clicks=0), 
 
     html.Div(children=[] , id="filter_zone"), 
 
@@ -16,15 +15,29 @@ app.layout = html.Div([
 ])
 
 
-callback(
+@callback(
     Output('filter_zone', 'children'), 
     Input('add_filter', 'n_clicks')
 )
 
 def filter_field (nClicks) : 
     patched_children = Patch()
-
     new_filter = dcc.Dropdown(id={'type' : 'city_filter' , 'index': nClicks}, options=options)
 
-    patched_children(new_filter); 
+    patched_children.append(new_filter); 
     return patched_children ; 
+
+
+@callback(
+        Output('outputContent', 'children'), 
+        Input({'type' : 'city_filter' , 'index': ALL}, 'value')
+)
+
+def outputValue(values) :
+     return html.Div(
+        [html.Div(f"Dropdown {i + 1} = {value}") for (i, value) in enumerate(values)]
+    )
+
+
+if(__name__ == "__main__") : 
+     app.run(debug=True)
