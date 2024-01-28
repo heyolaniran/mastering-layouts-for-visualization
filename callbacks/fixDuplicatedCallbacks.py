b05f40584af38350000fb568c0ebc3d5f78c2a60
heyolaniran <olaniran.abd@gmail.com>
2024-01-28 21:25:47 +0100

    duplicatedCallbacks error fix completed

--- callbacks/fixDuplicatedCallbacks.py
+++ callbacks/fixDuplicatedCallbacks.py
@@ -1,6 +1,6 @@
-from dash import Dash , dcc,  callback_context , html , callback, Input , Output
+from dash import Dash , dcc,  ctx , html , callback, Input , Output 
 import plotly_express as px 
-import pandas
+import plotly.graph_objects as go
 
 
 app = Dash(__name__); 
@@ -13,3 +13,33 @@ app.layout = html.Div([
 
     dcc.Graph(id="graph")
 ])
+
+@callback(
+    Output(component_id='graph', component_property='figure'),
+    inputs = [Input(component_id='draw_graph', component_property="n_clicks"), 
+              Input(component_id="reset_graph", component_property="n_clicks")], 
+    prevent_initial_call = True
+)
+
+
+def update_graph(button1 , button2) : 
+
+    action = ctx.triggered_id 
+
+    if(action =="reset_graph") : 
+        return reset()
+    else : 
+        return draw()
+
+
+def draw(): 
+    df = px.data.iris() ; 
+
+    return px.scatter(df , x=df.columns[0], y=df.columns[1])
+
+
+def reset() : 
+    return go.Figure() ; 
+
+if(__name__ == "__main__"): 
+    app.run(debug=True)
